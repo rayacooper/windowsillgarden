cd3a252002cb627c08e0109ddf72a9e3fece0f8e
rayacooper <47796461+rayacooper@users.noreply.github.com>
2020-04-23 22:03:25 -0600

    Trying to fix Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
+import Routes from './Routes'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        {Routes}
+      </Router>
     </div>
   );
 }
--- src/Routes.js
+++ src/Routes.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+import WindowSill from './Components/WindowSill/WindowSill';
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route component={WindowSill} path='/home'/>
+            <Route component={Register} path='/register'/>
+            <Route component={Login} path='/'/>
+        </Switch>
+    )
+}
+
+export default Routes;
